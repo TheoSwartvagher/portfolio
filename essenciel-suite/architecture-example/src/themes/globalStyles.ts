c807c37d688c263dfe8c332bf52d3adbff7a2a68
TheoSwartvagher <145830008+TheoSwartvagher@users.noreply.github.com>
2026-02-19 11:26:48 +0100

    Update globalStyles.ts

--- essenciel-suite/architecture-example/src/themes/globalStyles.ts
+++ essenciel-suite/architecture-example/src/themes/globalStyles.ts
@@ -1,1 +1,17 @@
-
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+});
