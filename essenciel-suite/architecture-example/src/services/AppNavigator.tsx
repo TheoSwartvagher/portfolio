8460a3ce2cf226da8a2c44f5679860db79264876
TheoSwartvagher <145830008+TheoSwartvagher@users.noreply.github.com>
2026-02-19 11:03:39 +0100

    Update and rename navigation.ts to AppNavigator.tsx

---
+++ essenciel-suite/architecture-example/src/services/AppNavigator.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { HomePage } from '../pages/Homepage';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
