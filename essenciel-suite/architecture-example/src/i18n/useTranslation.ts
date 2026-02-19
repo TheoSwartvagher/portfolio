2f36dcb5f7d87930873671e9e8a0a9f69357955d
TheoSwartvagher <145830008+TheoSwartvagher@users.noreply.github.com>
2026-02-19 11:33:57 +0100

    Update useTranslation.ts

--- essenciel-suite/architecture-example/src/i18n/useTranslation.ts
+++ essenciel-suite/architecture-example/src/i18n/useTranslation.ts
@@ -1,1 +1,15 @@
+import { useContext } from 'react';
+import { LanguageContext } from '../contexts/LanguageContext';
+import { t as i18nT } from './index';
+
+export const useTranslation = () => {
+  const { language } = useContext(LanguageContext);
+
+  // 👇 le simple fait de lire `language`
+  // force le re-render quand il change
+  return {
+    t: (key: string) => i18nT(key),
+    language,
+  };
+};
 
