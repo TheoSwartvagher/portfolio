6c235eafc5de76f32a6c2cfc02b2785ff09221aa
TheoSwartvagher <145830008+TheoSwartvagher@users.noreply.github.com>
2026-02-19 11:05:54 +0100

    Update App.tsx

--- essenciel-suite/architecture-example/App.tsx
+++ essenciel-suite/architecture-example/App.tsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { AppNavigator } from './src/services/AppNavigator';
+import { AppProvider } from './src/contexts/AppContext';
+import { LanguageProvider } from './src/contexts/LanguageContext';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <LanguageProvider>
+      <AppProvider>
+        <StatusBar />
+        <AppNavigator />
+      </AppProvider>
+      </LanguageProvider>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
