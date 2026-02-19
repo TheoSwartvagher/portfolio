a886945a982b7fc480e9ca8bbc4a941e18554263
TheoSwartvagher <145830008+TheoSwartvagher@users.noreply.github.com>
2026-02-19 11:06:42 +0100

    Update theme.ts

--- essenciel-suite/architecture-example/src/themes/theme.ts
+++ essenciel-suite/architecture-example/src/themes/theme.ts
@@ -1,1 +1,41 @@
+export const theme = {
+
+    colors:{
+primary: '#7E6BC4',        
+primarySoft: '#E6E2F5',
+background: '#F9F9FB',
+card: '#FFFFFF',
+text: '#222222',
+textMuted: '#7A7890',
+danger: '#E57373',
+border: '#E0E0E0',
+  },
+
+  spacing: {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+  },
+
+  radius: {
+    sm: 6,
+    md: 10,
+  },
+
+  typography: {
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+
+    sizes: {
+      sm: 14,
+      md: 16,
+      lg: 20,
+      xl: 24,
+    },
+  }
+};
+
 
